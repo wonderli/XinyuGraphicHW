42724cf637f1407ad95d23a7ea9d50c1fe9efde8
Xinyu Li <lixinyu2268@gmail.com>
2012-01-20 00:36:23 -0500

    finish camera setup

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -19,7 +19,7 @@ using namespace std;
 #include <Inventor/nodes/SoPointLight.h>
 
 #include "OSUInventor.h"
-
+#define PI 3.1415926536
 void usage_error();
 int main(int argc, char **argv) {
 	if (argc != 2)
@@ -28,6 +28,45 @@ int main(int argc, char **argv) {
 	OSUInventorScene *scene = new OSUInventorScene(argv[1]);
 	/*Set up camera*/
 	SoCamera *camera = NULL;
+        SbVec3f camera_position(0,0,1); //inital camera_position
+        SbRotation camera_orientation(0,0,1,0); //initial camera_orientation
+        SbVec3f camera_rotation_axis;
+        SbVec3f camera_direction;
+        SbVec3f camera_view_up;
+        float filed_view_angle = PI/4; //45 degree
+        float camera_roation_angle;
+        float camera_aspect_ratio = 1.0;
+        SoType camera_type;
+        /* 
+        * If camera is not defined, use the following default:
+        *         camera position = (0,0,1)
+        *         camera view up vector = (0,1,0)
+        *         camera looks at (0,0,0)
+        *         camera aspect ratio = 1.0 
+        *         camera field of view angle = 45 degree
+        * This is perspective camera.
+        */
+        if (scene->Camera == NULL) {
+                camera = new SoPerspectiveCamera();
+                camera_type = new SoPerspectiveCamera::getClassTypeId();
+        } else {
+                camera = scene->Camera;
+                camera_position = camera->position.getValue();
+                camera_orientation = camera->orientation.getValue();
+                camera_orientation.getValue(camera_rotation_axis, camera_roation_angle);
+                camera_aspect_ratio = camera->aspectRatio.getValue();
+                camera_orientation.multVec(SbVec3f(0, 0, -1), camera_direction);
+                camera_orientation.multVec(SbVec3f(0, 1, 0), camera_view_up);
+        }
+        SbVec3f eye, COI,n, v, u;
+        eye.setValue(camera_position[0], camera_position[1], camera_position[2]);
+        n = eye.normalize().negate(); // n setting
+        u = u.setValue(camera_view_up(cross(n).getValue)).normalize();
+        v = v.setValue(n.cross(u).getValue).normalize();
+
+
+
+
 
 
 
