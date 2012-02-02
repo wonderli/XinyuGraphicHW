2f0e1579dcd2f603206325b4e85b5fe85442c06a
Xinyu Li <lixinyu2268@gmail.com>
2012-02-01 22:06:12 -0500

    last version

--- Lab1/Lab1.C
+++ Lab1/Lab1.C
@@ -36,7 +36,7 @@ int main(int argc, char **argv) {
 	OSUInventorScene *scene = new OSUInventorScene(argv[1]);
         length = scene->Objects.getLength();
         SbMatrix *transform_list = new SbMatrix[length];
-	/*Set up camera*/
+	/* Set up camera */
 	SoCamera *camera = NULL;
         SbVec3f camera_position(0,0,1); //inital camera_position
         SbRotation camera_orientation(0,0,1,0); //initial camera_orientation
@@ -75,6 +75,7 @@ int main(int argc, char **argv) {
         camera_orientation.multVec(SbVec3f(0, 0, -1), camera_direction);
         camera_orientation.multVec(SbVec3f(0, 1, 0), camera_view_up);
 
+        /* Set up eye, n, v, u */
         SbVec3f eye, COI,n, v, u;
         eye.setValue(camera_position[0], camera_position[1], camera_position[2]);
         camera_direction.normalize();
@@ -85,9 +86,8 @@ int main(int argc, char **argv) {
         u.normalize();
         v.setValue(n.cross(u).getValue());
         v.normalize();
-        /*Calculation Resolution*/
-       // int yres = atoi(argv[3]);
-       // int xres = atoi(argv[4]);
+
+        /* Calculation Resolution */
         int xres = atoi(argv[3]);
         int yres = atoi(argv[4]);
         if(xres != (int)(yres*(camera->aspectRatio.getValue()))) {
@@ -96,9 +96,9 @@ int main(int argc, char **argv) {
                 cout << "Now the yres value is " << yres << endl;
         }
 
+        //int xres = (int)(yres*(camera->aspectRatio.getValue()));
 
-        //int xres = (int)(yres*(camera->aspectRatio.getValue()));
-        //int xres = (int)(yres*(camera->aspectRatio.getValue()));
+        /* Image plane setup */
         int d = 1;//d is 1
 
         float pixel_height;
@@ -109,18 +109,22 @@ int main(int argc, char **argv) {
                 pixel_height = 2 * tan(height_angle/2) * d /yres; //Calculate H, pixel height H/Y
                 pixel_width = pixel_height * yres * camera_aspect_ratio / xres; // Calculate pixel width W/X
         }
+
         pixel_center = eye - n * d;
         upperleft_corner = pixel_center - (xres/2) * pixel_width * u + (yres/2) * pixel_height *v;
 //        upperleft_corner = eye - n - (xres/2)*pixel_width*u + (yres/2)*pixel_height*v;
 //        pixel_center = upperleft_corner + pixel_width/2*u - pixel_height/2*v;
-        SbVec3f current, ray;
-        SbColor *color = new SbColor();
- //       scanline_start = upperleft_corner - pixel_height *v;
-        //scanline_start.setValue(pixel_center[0], pixel_center[1], pixel_center[2]);
-        scanline_start.setValue(upperleft_corner[0], upperleft_corner[1], upperleft_corner[2]);
 
 
-        //Initialize the ppm file
+        /*Initialize the ppm file
+        * PPM format is like the following:
+        * P3
+        * #comment
+        * xresolution yresolution
+        * 255
+        * The maximu pixel value
+        */
+
         fstream fp;
         fp.open(argv[2], fstream::out);
         if(!fp.is_open()){
@@ -131,64 +135,93 @@ int main(int argc, char **argv) {
         fp << "#" << argv[2] <<endl;
         fp << xres << ' ' << yres <<endl;
         fp << "255" << endl;
-        //Finish initialize file
-        //Call shader for each pixel
+        /* Finish initialize PPM file */
+
+        /* Begin ray tracing */
+        SbVec3f current, ray;
         int scanline, pixel;
+        SbColor *color = new SbColor();
         float r, g, b;
 
-       // float r, g, b;
-        //iterate
+        /* Set scanline start begin at the left corner */
+        scanline_start.setValue(upperleft_corner[0], upperleft_corner[1], upperleft_corner[2]);
+        /* Begin to do the iterate */
         for(scanline = 0; scanline < yres; scanline++) {
                 current = scanline_start;
 
                 for (pixel = 0; pixel < xres; pixel++) {
                         ray = current - eye;
                         ray.normalize();
+
                         ray_trace(ray, eye, scene, transform_list, color);
+
                         r = (*color)[0];
                         g = (*color)[1];
                         b = (*color)[2];
+                        /* Print to file*/
                         fp << (int)r << ' ' << (int)g << ' ' <<(int)b <<endl;
+                        /* Move forward to next pixel*/
                         current += pixel_width*u;
                 }
+
                 scanline_start -= pixel_height*v;
+
         }
         fp.close();
 	return 0;
-
 }
+/*
+* Name: usage_error
+* Argument: void
+* Return value: void
+* Usage: Show the program usage to the user.
+*/
+
 void usage_error() {
 	cerr << "Usage: rt <input.iv> <output.ppm> <xres> <yres>" << endl;
 	exit(10);
 }
+/*
+* Name: set_object
+* Argument: 
+*               scene: OSUInventorScene 
+*               transforam_list: Transform matrix list, which contains every object
+* Usage: Get the object transformation information, and store these in the transform_list
+*/
 void set_object(OSUInventorScene *scene, SbMatrix *transform_list) {
         OSUObjectData *object = NULL;
         int length;
+
         SbMatrix T, S, R;
+
         SoTransform *transformation = NULL;
         SbVec3f scale_vector;
         SbRotation rotation;
         SbVec3f rotation_axis;
         float rotation_angle;
         SbVec3f translation_vector;
+
         length = scene->Objects.getLength();
-        int i = 0;
+
         if(!scene) {
                 cerr << "Scene is empty!" <<endl;
                 exit(0);
 
         } 
         if(!transform_list) {
-                cerr << "Tansform List not normal" << endl;
+                cerr << "Tansform List error" << endl;
                 exit(0);
         }
-        for (i = 0; i < length; i++){
+
+        int i = 0;
+        for (i = 0; i < length; i++) {
                 //OSUObjectData *object = (OSUObjectData *)scene->Objects[i];
                 object = (OSUObjectData *)scene->Objects[i];
                 if(!object->Check()) {
                         cerr << "Error in OSUObjectData for object" << i << endl;
                         exit(0);
                 }
+
                 transformation = object->transformation;
                 translation_vector = transformation->translation.getValue();
                 scale_vector = transformation->scaleFactor.getValue();
@@ -209,7 +242,17 @@ void set_object(OSUInventorScene *scene, SbMatrix *transform_list) {
                 transform_list[i].multRight(T);
         }
 }
-
+/* 
+* Name: sphere_intersect
+* Argument: 
+*          ray: ray vector shooting from eye to the center of each pixel
+*          eye: eye position
+*          sphere: sphere need to be tested
+*          point_intersect: the intersection point coordinate.*
+* Return value: 1 means intersect with the sphere, -1 mesans have no intersect with the sphere
+* Usage:
+*       Check the ray and sphere whether have intersection or not, if have, store the intersection point.
+*/
 
 
 int sphere_intersect(SbVec3f ray, SbVec3f eye, SbSphere sphere, SbVec3f *point_intersect) {
@@ -218,19 +261,14 @@ int sphere_intersect(SbVec3f ray, SbVec3f eye, SbSphere sphere, SbVec3f *point_i
         float a, b, c, discriminant;
         float root_1, root_2;
         float root;
-//        SbVec3f d = eye - sphere_center;
         int is_intersect = -1;
-//        a = 1;
-//        b = 2 * d.dot(ray);
-//        c = d.dot(eye - sphere_center) - r * r;
-//        c = d.dot(d) - r * r;
         SbVec3f d = ray;
-       // d.normalize();
         SbVec3f eye_minus_center = eye - sphere_center;
 
         a = d.dot(d);
         b = 2 * d.dot(eye - sphere_center);
         c = eye_minus_center.dot(eye_minus_center) - r * r;
+
         discriminant = b * b - 4 * a * c;
         if(discriminant > ZERO) {
                 root_1 = (-b - sqrt(discriminant))/(2*a);
@@ -270,19 +308,39 @@ int sphere_intersect(SbVec3f ray, SbVec3f eye, SbSphere sphere, SbVec3f *point_i
         return is_intersect;
 }
 
+
+/*
+* Name: ray_trace
+* Argument:
+*          ray: ray vector shooting from eye to the center of each pixel
+*          eye: eye position
+*          scene: scene info
+*          transform_list: Transform list from set_object function
+*          color: color info at each pixel
+* Return vaule: void
+* Usage:
+*       Ray tracing main algorithm. Get the closest object intersection point, and set the corresponding color at that pixel.
+*
+*
+*/
+
 void ray_trace(SbVec3f ray, SbVec3f eye, OSUInventorScene *scene, SbMatrix *transform_list, SbColor *color) {
         int length = scene->Objects.getLength();
-        int i;
         SbVec3f center(0, 0, 0);
         SbVec3f center_new(0, 0, 0);
         SbVec3f center_min(0, 0, 0);
+
         SbVec3f *point_intersect = new SbVec3f();
+
         SbVec3f distance;
         float distance_length;
         float distance_length_min = FAR;
+
         float radius = 1;
+
         OSUObjectData *object = new OSUObjectData();
         OSUObjectData *closest_object = new OSUObjectData();
+
         float r, g, b;
         SoType shape_type;
         SbVec3f scale_vector;
@@ -291,11 +349,13 @@ void ray_trace(SbVec3f ray, SbVec3f eye, OSUInventorScene *scene, SbMatrix *tran
         int is_intersect = -1;
         float z_component;
 
+        int i;
         for (i = 0; i < length; i++) {
                 object = (OSUObjectData *)scene->Objects[i];
                 shape_type = object->shape->getTypeId(); //Get Object ID
 
                 if(shape_type == SoSphere::getClassTypeId()) {
+                        /* Transform object */
                         center.setValue(0, 0, 0);
                         transform_list[i].multVecMatrix(center, center_new);
                         scale_vector = object->transformation->scaleFactor.getValue();
@@ -307,7 +367,7 @@ void ray_trace(SbVec3f ray, SbVec3f eye, OSUInventorScene *scene, SbMatrix *tran
                                 distance_length = distance.length();
                         } else
                         distance_length = FAR;
-
+                        /* Update the closest object */
                         if(distance_length < distance_length_min) {
                                 closest_object = object;
                                 distance_length_min = distance_length;
@@ -325,18 +385,9 @@ void ray_trace(SbVec3f ray, SbVec3f eye, OSUInventorScene *scene, SbMatrix *tran
                 }else if (z_component < 0){
                         z_component = 0;
                 }
-
-                r = closest_object->material->diffuseColor[0][0];
-                g = closest_object->material->diffuseColor[0][1];
-                b = closest_object->material->diffuseColor[0][2];
-
-                r *= z_component;
-                g *= z_component;
-                b *= z_component;
-
-                r *= 255;
-                g *= 255;
-                b *= 255;
+                r = closest_object->material->diffuseColor[0][0] * z_component * 255;
+                g = closest_object->material->diffuseColor[0][1] * z_component * 255;
+                b = closest_object->material->diffuseColor[0][2] * z_component * 255;
         } else {
                 r = 0;
                 g = 0;
