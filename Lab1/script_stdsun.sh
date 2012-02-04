c3f7bf023522540564a7389ec6029e2483122b78
Xinyu Li <lixinyu2268@gmail.com>
2012-02-04 00:09:31 -0500

    test

--- Lab1/script_stdsun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-make clean
-make 
-./rt sphere1.iv sphere1.ppm 500 500 
-./rt sphere3.iv sphere3.ppm 500 500 
-./rt mickeyspheres.iv mickeyspheres.ppm 500 500 
-./rt sphereTower.iv sphereTower.ppm 500 500 
