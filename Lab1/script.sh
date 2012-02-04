5513587de08eb50c0842fab5246bc479ff20f0a0
Xinyu Li <lixinyu2268@gmail.com>
2012-02-04 00:12:13 -0500

    test

---
+++ Lab1/script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+make clean
+make -f Makefile.linux
+./rt sphere1.iv sphere1.ppm 500 500 
+./rt sphere3.iv sphere3.ppm 500 500 
+./rt mickeyspheres.iv mickeyspheres.ppm 500 500 
+./rt sphereTower.iv sphereTower.ppm 500 500 
