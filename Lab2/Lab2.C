71e2b7d3f8870ffc346bbd754df4ecec1443c54e
Xinyu Li <lixinyu2268@gmail.com>
2012-02-06 18:58:30 -0500

    rearrange

--- Lab2/Lab2.C
+++ Lab2/Lab2.C
@@ -26,152 +26,6 @@ using namespace std;
 #define PI 3.1415926536
 #define ZERO 1e-6
 #define FAR 1e10
-void usage_error();
-void set_object(OSUInventorScene *scene, SbMatrix *transform_list);
-int sphere_intersect(SbVec3f ray, SbVec3f eye, SbSphere sphere, SbVec3f *point_intersect);
-void ray_trace(SbVec3f ray, SbVec3f eye, OSUInventorScene *scene, SbMatrix *transform_list, SbColor *shadeColor);
-int main(int argc, char **argv) {
-	if (argc < 5)
-		usage_error();
-	SoDB::init();
-        int length;
-	OSUInventorScene *scene = new OSUInventorScene(argv[1]);
-        length = scene->Objects.getLength();
-        SbMatrix *transform_list = new SbMatrix[length];
-	/* Set up camera */
-	SoCamera *camera = NULL;
-        SbVec3f camera_position(0,0,1); //inital camera_position
-        SbRotation camera_orientation(0,0,1,0); //initial camera_orientation
-        SbVec3f camera_rotation_axis;
-        SbVec3f camera_direction;
-        SbVec3f camera_view_up;
-        float height_angle = PI/4; //45 degree
-        float camera_roation_angle;
-        float camera_aspect_ratio = 1.0;
-        SoType camera_type;
-        set_object(scene, transform_list);//set up object list
-        /* 
-        * If camera is not defined, use the following default:
-        *         camera position = (0,0,1)
-        *         camera view up vector = (0,1,0)
-        *         camera looks at (0,0,0)
-        *         camera aspect ratio = 1.0 
-        *         camera field of view angle = 45 degree
-        * This is perspective camera.
-        */
-        if (scene->Camera == NULL) {
-                camera = new SoPerspectiveCamera();
-                camera_type =  SoPerspectiveCamera::getClassTypeId();
-        } else {
-                camera = scene->Camera;
-                camera_position = camera->position.getValue();
-                camera_orientation = camera->orientation.getValue();
-                camera_orientation.getValue(camera_rotation_axis, camera_roation_angle);
-                camera_aspect_ratio = camera->aspectRatio.getValue();
-                camera_type = camera->getTypeId();
-                if (camera_type == SoPerspectiveCamera::getClassTypeId()) {
-                        SoPerspectiveCamera *perspective_camera = (SoPerspectiveCamera *)camera;
-                        height_angle = perspective_camera->heightAngle.getValue();
-                }
-        }
-        camera_orientation.multVec(SbVec3f(0, 0, -1), camera_direction);
-        camera_orientation.multVec(SbVec3f(0, 1, 0), camera_view_up);
-
-        /* Set up eye, n, v, u */
-        SbVec3f eye, COI,n, v, u;
-        eye.setValue(camera_position[0], camera_position[1], camera_position[2]);
-        camera_direction.normalize();
-        n.setValue(camera_direction[0], camera_direction[1], camera_direction[2]);
-        n.negate(); // n setting
-        camera_view_up.normalize();
-        u.setValue(camera_view_up.cross(n).getValue());
-        u.normalize();
-        v.setValue(n.cross(u).getValue());
-        v.normalize();
-
-        /* Calculation Resolution */
-        int xres = atoi(argv[3]);
-        int yres = atoi(argv[4]);
-        if(xres != (int)(yres*(camera->aspectRatio.getValue()))) {
-                cout << "yres and xres value should follow the aspect ratio" << endl;
-                yres = xres/(camera->aspectRatio.getValue());
-                cout << "Now the yres value is " << yres << endl;
-        }
-
-        //int xres = (int)(yres*(camera->aspectRatio.getValue()));
-
-        /* Image plane setup */
-        int d = 1;//d is 1
-
-        float pixel_height;
-        float pixel_width;
-        
-        SbVec3f upperleft_corner, pixel_center, scanline_start;
-        if (camera_type == SoPerspectiveCamera::getClassTypeId()) {
-                pixel_height = 2 * tan(height_angle/2) * d /yres; //Calculate H, pixel height H/Y
-                pixel_width = pixel_height * yres * camera_aspect_ratio / xres; // Calculate pixel width W/X
-        }
-
-        pixel_center = eye - n * d;
-        upperleft_corner = pixel_center - (xres/2) * pixel_width * u + (yres/2) * pixel_height *v;
-//        upperleft_corner = eye - n - (xres/2)*pixel_width*u + (yres/2)*pixel_height*v;
-//        pixel_center = upperleft_corner + pixel_width/2*u - pixel_height/2*v;
-
-
-        /*Initialize the ppm file
-        * PPM format is like the following:
-        * P3
-        * #comment
-        * xresolution yresolution
-        * 255
-        * The maximu pixel value
-        */
-
-        fstream fp;
-        fp.open(argv[2], fstream::out);
-        if(!fp.is_open()){
-                cerr << "Error in writing file" << argv[2] <<endl;
-                exit(0);
-        }
-        fp << "P3"<<endl;
-        fp << "#" << argv[2] <<endl;
-        fp << xres << ' ' << yres <<endl;
-        fp << "255" << endl;
-        /* Finish initialize PPM file */
-
-        /* Begin ray tracing */
-        SbVec3f current, ray;
-        int scanline, pixel;
-        SbColor *color = new SbColor();
-        float r, g, b;
-
-        /* Set scanline start begin at the left corner */
-        scanline_start.setValue(upperleft_corner[0], upperleft_corner[1], upperleft_corner[2]);
-        /* Begin to do the iterate */
-        for(scanline = 0; scanline < yres; scanline++) {
-                current = scanline_start;
-
-                for (pixel = 0; pixel < xres; pixel++) {
-                        ray = current - eye;
-                        ray.normalize();
-
-                        ray_trace(ray, eye, scene, transform_list, color);
-
-                        r = (*color)[0];
-                        g = (*color)[1];
-                        b = (*color)[2];
-                        /* Print to file*/
-                        fp << (int)r << ' ' << (int)g << ' ' <<(int)b <<endl;
-                        /* Move forward to next pixel*/
-                        current += pixel_width*u;
-                }
-
-                scanline_start -= pixel_height*v;
-
-        }
-        fp.close();
-	return 0;
-}
 /*
 * Name: usage_error
 * Argument: void
@@ -486,3 +340,146 @@ void ray_trace(SbVec3f ray, SbVec3f eye, OSUInventorScene *scene, SbMatrix *tran
         (*color).setValue(r, g, b);
 
 }
+int main(int argc, char **argv) {
+	if (argc < 5)
+		usage_error();
+	SoDB::init();
+        int length;
+	OSUInventorScene *scene = new OSUInventorScene(argv[1]);
+        length = scene->Objects.getLength();
+        SbMatrix *transform_list = new SbMatrix[length];
+	/* Set up camera */
+	SoCamera *camera = NULL;
+        SbVec3f camera_position(0,0,1); //inital camera_position
+        SbRotation camera_orientation(0,0,1,0); //initial camera_orientation
+        SbVec3f camera_rotation_axis;
+        SbVec3f camera_direction;
+        SbVec3f camera_view_up;
+        float height_angle = PI/4; //45 degree
+        float camera_roation_angle;
+        float camera_aspect_ratio = 1.0;
+        SoType camera_type;
+        set_object(scene, transform_list);//set up object list
+        /* 
+        * If camera is not defined, use the following default:
+        *         camera position = (0,0,1)
+        *         camera view up vector = (0,1,0)
+        *         camera looks at (0,0,0)
+        *         camera aspect ratio = 1.0 
+        *         camera field of view angle = 45 degree
+        * This is perspective camera.
+        */
+        if (scene->Camera == NULL) {
+                camera = new SoPerspectiveCamera();
+                camera_type =  SoPerspectiveCamera::getClassTypeId();
+        } else {
+                camera = scene->Camera;
+                camera_position = camera->position.getValue();
+                camera_orientation = camera->orientation.getValue();
+                camera_orientation.getValue(camera_rotation_axis, camera_roation_angle);
+                camera_aspect_ratio = camera->aspectRatio.getValue();
+                camera_type = camera->getTypeId();
+                if (camera_type == SoPerspectiveCamera::getClassTypeId()) {
+                        SoPerspectiveCamera *perspective_camera = (SoPerspectiveCamera *)camera;
+                        height_angle = perspective_camera->heightAngle.getValue();
+                }
+        }
+        camera_orientation.multVec(SbVec3f(0, 0, -1), camera_direction);
+        camera_orientation.multVec(SbVec3f(0, 1, 0), camera_view_up);
+
+        /* Set up eye, n, v, u */
+        SbVec3f eye, COI,n, v, u;
+        eye.setValue(camera_position[0], camera_position[1], camera_position[2]);
+        camera_direction.normalize();
+        n.setValue(camera_direction[0], camera_direction[1], camera_direction[2]);
+        n.negate(); // n setting
+        camera_view_up.normalize();
+        u.setValue(camera_view_up.cross(n).getValue());
+        u.normalize();
+        v.setValue(n.cross(u).getValue());
+        v.normalize();
+
+        /* Calculation Resolution */
+        int xres = atoi(argv[3]);
+        int yres = atoi(argv[4]);
+        if(xres != (int)(yres*(camera->aspectRatio.getValue()))) {
+                cout << "yres and xres value should follow the aspect ratio" << endl;
+                yres = xres/(camera->aspectRatio.getValue());
+                cout << "Now the yres value is " << yres << endl;
+        }
+
+        //int xres = (int)(yres*(camera->aspectRatio.getValue()));
+
+        /* Image plane setup */
+        int d = 1;//d is 1
+
+        float pixel_height;
+        float pixel_width;
+        
+        SbVec3f upperleft_corner, pixel_center, scanline_start;
+        if (camera_type == SoPerspectiveCamera::getClassTypeId()) {
+                pixel_height = 2 * tan(height_angle/2) * d /yres; //Calculate H, pixel height H/Y
+                pixel_width = pixel_height * yres * camera_aspect_ratio / xres; // Calculate pixel width W/X
+        }
+
+        pixel_center = eye - n * d;
+        upperleft_corner = pixel_center - (xres/2) * pixel_width * u + (yres/2) * pixel_height *v;
+//        upperleft_corner = eye - n - (xres/2)*pixel_width*u + (yres/2)*pixel_height*v;
+//        pixel_center = upperleft_corner + pixel_width/2*u - pixel_height/2*v;
+
+
+        /*Initialize the ppm file
+        * PPM format is like the following:
+        * P3
+        * #comment
+        * xresolution yresolution
+        * 255
+        * The maximu pixel value
+        */
+
+        fstream fp;
+        fp.open(argv[2], fstream::out);
+        if(!fp.is_open()){
+                cerr << "Error in writing file" << argv[2] <<endl;
+                exit(0);
+        }
+        fp << "P3"<<endl;
+        fp << "#" << argv[2] <<endl;
+        fp << xres << ' ' << yres <<endl;
+        fp << "255" << endl;
+        /* Finish initialize PPM file */
+
+        /* Begin ray tracing */
+        SbVec3f current, ray;
+        int scanline, pixel;
+        SbColor *color = new SbColor();
+        float r, g, b;
+
+        /* Set scanline start begin at the left corner */
+        scanline_start.setValue(upperleft_corner[0], upperleft_corner[1], upperleft_corner[2]);
+        /* Begin to do the iterate */
+        for(scanline = 0; scanline < yres; scanline++) {
+                current = scanline_start;
+
+                for (pixel = 0; pixel < xres; pixel++) {
+                        ray = current - eye;
+                        ray.normalize();
+
+                        ray_trace(ray, eye, scene, transform_list, color);
+
+                        r = (*color)[0];
+                        g = (*color)[1];
+                        b = (*color)[2];
+                        /* Print to file*/
+                        fp << (int)r << ' ' << (int)g << ' ' <<(int)b <<endl;
+                        /* Move forward to next pixel*/
+                        current += pixel_width*u;
+                }
+
+                scanline_start -= pixel_height*v;
+
+        }
+        fp.close();
+	return 0;
+}
+
