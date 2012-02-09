45f1b595cc0710c49f6a71bb104e1950339ea471
Xinyu Li <lixinyu2268@gmail.com>
2012-02-08 19:37:34 -0500

    little

--- Lab2/script.sh
+++ Lab2/script.sh
@@ -2,6 +2,7 @@
 make clean
 make -f Makefile.linux
 #./rt sphere1.iv sphere1.ppm 500 500 
-#./rt sphere3.iv sphere3.ppm 500 500 
+./rt ssphere3t.iv sphere3t.ppm 500 500 
 ./rt mickeyspheres.iv mickeyspheres.ppm 100 100
+./rt sphereCycloid.iv sphereCycloid.ppm 100 100
 #./rt sphereTower.iv sphereTower.ppm 500 500 
