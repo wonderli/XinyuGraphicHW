94406ae1fc31a2bde942ad4b87f805eab529e185
Xinyu Li <lixinyu2268@gmail.com>
2012-02-09 20:24:57 -0500

    add sphere3s.iv

--- Lab2/script.sh
+++ Lab2/script.sh
@@ -5,6 +5,6 @@ make -f Makefile.linux
 #./rt sphere3t.iv sphere3t.ppm 500 500 
 #./rt sphere3s.iv sphere3s.ppm 500 500 
 ./rt mickeyspheres.iv mickeyspheres.ppm 500 500
-./rt sphereCycloid.iv sphereCycloid.ppm 500 500
+#./rt sphereCycloid.iv sphereCycloid.ppm 500 500
 #./rt sphereCycloidLit.iv sphereCycloidLit.ppm 500 500
 #./rt sphereTower.iv sphereTower.ppm 500 500 
