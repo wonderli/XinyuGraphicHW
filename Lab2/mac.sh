01ef4611bd9ada1667587a2443d504ad9304e7ee
Xinyu Li <lixinyu2268@gmail.com>
2012-02-09 20:28:03 -0500

    little

---
+++ Lab2/mac.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+make clean
+make -f Makefile.mac
+#./rt sphere1.iv sphere1.ppm 500 500 
+#./rt sphere3.iv sphere3.ppm 500 500 
+./rt sphere3s.iv sphere3s.ppm 500 500 
+./rt mickeyspheres.iv mickeyspheres.ppm 500 500
+./rt sphereCycloidLit.iv sphereCycloidLit.ppm 500 500
+
