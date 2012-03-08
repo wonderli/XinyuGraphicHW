b69846c512c13ab268e5f796336b81deb4b2d8d9
Xinyu Li <lixinyu2268@gmail.com>
2012-03-08 02:03:59 -0500

    add sphere, but strange

--- Lab4/MyRayTracer.h
+++ Lab4/MyRayTracer.h
@@ -31,32 +31,33 @@ using namespace std;
 #define RAY_OUTSIDE 1
 #define RAY_INSIDE 0
 class MyRayTracer{
-        public:
+	public:
 
-        /* Light */
-        SoLight *light;
-        SoType light_type; 
-        SbVec3f light_color;
-        SbVec3f ambient_color;
-        SbVec3f diffuse_color;
-        SbVec3f specular_color;        
-        float Kd;//diffuse coefficient
-        float light_intensity;
-        SbVec3f light_location;
-        SbVec3f light_vector;
-        SbVec3f light_direction;
+		/* Light */
+		SoLight *light;
+		SoType light_type; 
+		SbVec3f light_color;
+		SbVec3f ambient_color;
+		SbVec3f diffuse_color;
+		SbVec3f specular_color;        
+		float Kd;//diffuse coefficient
+		float light_intensity;
+		SbVec3f light_location;
+		SbVec3f light_vector;
+		SbVec3f light_direction;
 
 
 
-        MyRayTracer(OSUInventorScene *scene);
-        int sphere_intersect(SbVec3f ray, SbVec3f eye, SbSphere sphere, SbVec3f *point_intersect);
-        void rt(SbVec3f ray, SbVec3f eye, OSUInventorScene *scene, SbMatrix *transform_list, SbVec3f *color, int reflection_depth, int refraction_depth, int shadow_on, int reflection_on, int refraction_on, int ray_location);
+		MyRayTracer(OSUInventorScene *scene);
+		//        int sphere_intersect(SbVec3f ray, SbVec3f eye, SbSphere sphere, SbVec3f *point_intersect);
+		float sphere_intersect(SbVec3f ray, SbVec3f eye, SoSphere *sphere, SbMatrix transform_matrix, SbVec3f &point_intersect, SbVec3f &inter_normal);
+		void rt(SbVec3f ray, SbVec3f eye, OSUInventorScene *scene, SbMatrix *transform_list, SbVec3f *color, int reflection_depth, int refraction_depth, int shadow_on, int reflection_on, int refraction_on, int ray_location);
 
-//	int is_in_shadow(SbVec3f intersect_point, SbVec3f light, SbVec3f light_location, OSUInventorScene *scene, SbMatrix *transform_list);
+		//	int is_in_shadow(SbVec3f intersect_point, SbVec3f light, SbVec3f light_location, OSUInventorScene *scene, SbMatrix *transform_list);
 
-        int refract(SbVec3f d, SbVec3f n, float Eta, SbVec3f *t);
+		int refract(SbVec3f d, SbVec3f n, float Eta, SbVec3f *t);
 
-	float cube_intersect(SbVec3f ray, SbVec3f eye, SoCube *cube, SbMatrix transform_matrix, SbVec3f &point, SbVec3f &inter_normal);
-	float object_in_path(SbVec3f intersect_point, SbVec3f light_vector, SbVec3f light_location, SbMatrix *transform_list, OSUInventorScene *scene,float t1, float t2);
-	void coordinate_gen(SbVec3f light_vector, SbVec3f &u, SbVec3f &v, SbVec3f &w);
+		float cube_intersect(SbVec3f ray, SbVec3f eye, SoCube *cube, SbMatrix transform_matrix, SbVec3f &point, SbVec3f &inter_normal);
+		float object_in_path(SbVec3f intersect_point, SbVec3f light_vector, SbVec3f light_location, SbMatrix *transform_list, OSUInventorScene *scene,float t1, float t2);
+		void coordinate_gen(SbVec3f light_vector, SbVec3f &u, SbVec3f &v, SbVec3f &w);
 };
